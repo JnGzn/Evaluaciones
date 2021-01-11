f0809c17fc322ac5afad5ca6963cb526c64fc16b
jngzn <jngzn@hotmail.com>
2021-01-10 23:39:49 -0500

    added guard

--- simulacro/src/app/app-routing.module.ts
+++ simulacro/src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ExamenComponent } from './components/examen/examen.component';
 import { CrearPreguntasComponent } from './components/crear-preguntas/crear-preguntas.component';
+import { GuardGuard } from './services/guard.guard';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'home', component: HomeComponent},
-  {path: 'examen', component: ExamenComponent},
+  {path: 'examen', component: ExamenComponent, canActivate: [GuardGuard ]},
   {path: 'crearPregunta', component: CrearPreguntasComponent},
 
   {path: '**', redirectTo: 'home'}
